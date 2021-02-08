df95418227b31a1f268c024f0a7be1416df22769
sudhanthiran <sudhanthiran.bhaskar@gmail.com>
2021-02-08 18:37:52 +0530

    Social Connectivity code

---
+++ Competitive Coding/Social Connectivity.py
@@ -0,0 +1,47 @@
+## 5 funsctions, no external storgae, speed is the key O()
+### add, remove, friend and unfriend. info for (Social connectivity)
+## add (Name, City)
+## remove(Name)
+### friend (Name1, target_Name)
+#### unfriend (target_name)
+### info(Name)
+import typing
+
+
+class person():
+    def __init__(self,Name : str, City : str):
+        self.Name = Name
+        self.City = City
+        self.user_list = []
+    def add(self, Name, user_name):
+        self.user_list.append(user_name.Name)  ## need to handle duplicates
+    def remove(self, user_to_remove: str):
+        index_to_del = self.user_list.index(user_to_remove)
+        self.user_list.__delitem__(index_to_del)
+
+
+class FB_Dummy(person):
+    def __init__(self,Name, City):
+        super().__init__(Name,City)
+        self.friend_list = []
+    def add(self,object):
+        super().add(self.Name,object.Name)
+    def friend(self, friend_to_add: str):
+        self.friend_list.append(friend_to_add.Name)  ## need to handle duplicates
+    def unfriend(self, friend_to_remove: str):
+        index_to_del = self.friend_list.index(friend_to_remove)
+        self.friend_list.__delitem__(index_to_del)
+
+    def info(self):
+        return "Name: "+self.Name+" City: "+self.City
+
+
+obj1 = FB_Dummy("tom","nyc")
+obj2 = FB_Dummy("sam","tokyo")
+#obj3 = FB_Dummy("bob","london")
+
+obj1.friend(obj2)
+#obj1.add("bob")
+
+print(obj1.friend_list)
+
---
+++ Competitive Coding/Student Punishment.py
@@ -0,0 +1,5 @@
+####Data is stored in 2 different list
+#### Student contains (Student_ID,Student_Name,Class,Division)
+#### Data Table contains (No, PID,Stud_ID)
+#### Offense Data contains (PID,Offence_Description)
+
